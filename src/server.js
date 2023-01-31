7fcfdc2f326f4171b58eaa6b167976ed67c717b8
Felipe Lima <mrfelipelima@gmail.com>
2023-01-31 02:18:15 -0300

    added nodejs engine on package file

--- src/server.js
+++ src/server.js
@@ -1,25 +1,25 @@
-require("dotenv").config({
-  path: "./.env",
-});
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const cors = require("cors");
+require('dotenv').config({
+  path: './.env'
+})
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-io.on("connection", (socket) => {
-  socket.on("connectRoom", (box) => {
-    socket.join(box);
-  });
-});
+io.on('connection', (socket) => {
+  socket.on('connectRoom', (box) => {
+    socket.join(box)
+  })
+})
 
-console.log("Server started");
+console.log('Server started')
 
 mongoose.connect(
   `mongodb+srv://${process.env.MONGO_USERNAME}:${
@@ -28,20 +28,20 @@ mongoose.connect(
     process.env.MONGO_DB
   }?retryWrites=true&w=majority`,
   {
-    useNewUrlParser: true,
+    useNewUrlParser: true
   }
-);
+)
 
 app.use((req, res, next) => {
-  req.io = io;
+  req.io = io
 
-  return next();
-});
+  return next()
+})
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/files", express.static(path.resolve(__dirname, "..", "temp")));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')))
 
-app.use(require("./routes"));
+app.use(require('./routes'))
 
-server.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333)
